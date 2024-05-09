1c531e30abe94b6314f4be2cb48c247ff06b2eab
MarkoP00 <135166893+MarkoP00@users.noreply.github.com>
2024-05-09 21:19:46 +0200

    vue baseUrl depricated. changed to publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/social-media/" : "/",
-  baseUrl: process.env.NODE_ENV === "production" ? "/social-media/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/social-media/" : "/"
 };
